ff67552c36912b0b886b056be9279c20e64788f8
Zohaib <ranjha5054@gmail.com>
2026-09-12 18:29:56 +0500

    done

--- contexts/QuizContext.tsx
+++ contexts/QuizContext.tsx
@@ -7,7 +7,17 @@ export const QuizContext = createContext()
 
 export const QuizProvider = ({children}) => {
     const [quizes, setQuizes] = useState([])
-    const [currentQuiz, setCurrentQuiz] = useState(null)
+    const [currentQuiz, setCurrentQuiz] = useState({
+  topic: "",
+  length: 0,
+  difficulty: "Medium",
+  format: "Multiple Choice",
+  completed: false,
+  timer: 30,
+  score: 0,
+  accuracy: 0,
+  quiz: []
+})
 
     return <QuizContext.Provider value={{ quizes, setQuizes, currentQuiz, setCurrentQuiz }}>
         {children}
